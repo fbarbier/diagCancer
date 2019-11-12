b58e86b5d1986c0ea31666b1902f512d545fc732
Fabien Barbier <fabien.barbier@gmail.com>
2019-11-12 16:51:52 +0100

    Spring Boot Initializr
    Install spring boot dependancies via MAVEN

---
+++ src/main/java/fr/bordeaux/isped/sitis/diagCancer/DiagCancerApplication.java
@@ -0,0 +1,13 @@
+package fr.bordeaux.isped.sitis.diagCancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagCancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagCancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/bordeaux/isped/sitis/diagCancer/DiagCancerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bordeaux.isped.sitis.diagCancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagCancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
